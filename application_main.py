bb1b12b98425b0af0baa890c1940f2d4f5c33bc4
Shubham Bhargava <shubhambhargava05@gmail.com>
2025-07-07 19:17:32 +0530

    Feature1 to add logging level implementation in Spark code

--- application_main.py
+++ application_main.py
@@ -1,6 +1,7 @@
 import sys
 from lib import DataManipulation, DataReader, Utils
 from pyspark.sql.functions import *
+from lib.logger import Log4j
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
@@ -10,7 +11,8 @@ if __name__ == '__main__':
     job_run_env = sys.argv[1]
     print("Creating Spark Session")
     spark = Utils.get_spark_session(job_run_env)
-    print("Created Spark Session")
+    logger = Log4j(spark)
+    logger.warn("Created Spark Session")
 
     orders_df = DataReader.read_orders(spark,job_run_env)
     
@@ -19,4 +21,4 @@ if __name__ == '__main__':
     joined_df = DataManipulation.join_orders_customers(orders_filtered,customers_df)
     aggregated_results = DataManipulation.count_orders_state(joined_df)
     aggregated_results.show()
-    print("end of main")
+    logger.info("end of main")
